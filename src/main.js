1bd66bee2d78a5858a1b09e7341932c81950faab
magdalenaa sanhueza <magdasanhueza.s@gmail.com>
2021-07-19 11:59:34 -0400

    singIn

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { home } from './lib/view/TemplateHome.js';
+import { home } from "./lib/view/TemplateHome.js";
 
-home()
-
-const init = () =>{
-  document.getElementById('root').appendChild(home());
-
+const init = () => {
+  document.getElementById("root").appendChild(home());
 };
-window.addEventListener('load',init);
+window.addEventListener("load", init);
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+const inicioSesionGoogle = () => {
+  console.log("inicioSesionGoogle");
+  return firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+setTimeout(() => {
+  let buttonGoogle = document.getElementById("google");
+console.log('buttonGoogle: ', buttonGoogle)
+buttonGoogle.addEventListener("click", inicioSesionGoogle, false);
+}, 1000);
