f6f2df9b76f7a0a74a8c2e14a13449a5e268efe8
magdalenaa sanhueza <magdasanhueza.s@gmail.com>
2021-07-28 08:53:12 -0400

    Css templateWall

--- src/lib/view/templateWall.js
+++ src/lib/view/templateWall.js
@@ -1,28 +1,32 @@
-import {logOut} from './../logOut.js';
+import { logOut } from "./../logOut.js";
 
-export const wall = e => {
-
-    const wallHome = document.createElement('div');
-    const viewWall = `
+export const wall = (e) => {
+  const wallHome = document.createElement("div");
+  const viewWall = `
     <div class="header">
-        <h1>Barra superior</h1>
-        <button id="logOut">Cerrar Sesión </button>
+        <h2>Eluney</h2>
+        <h3>No lo utilizo, te lo regalo!</h3>
+        <div class= "containerBoxGreenOne">
+        <img class="boxGreenOne" src="Assets/sostenible (1).png">
+
+         </div>
+         <div class= "containerButton">
+        <button class ="buttonLogOut" id="logOut">Cerrar Sesión </button>
         </div>
     
     <div class="wallContainer">
      <div class="postOne">¿Qué Regalas?</div>
-     <div class="postTwo">¿Qué Regalas?</div>
+     
     </div>
     <a href="#/post"> Postear mensaje     
     </a>
     <div id='postFull'></div>
     `;
 
-    wallHome.innerHTML = viewWall;
+  wallHome.innerHTML = viewWall;
 
-    const buttonLogOut = wallHome.querySelector('#logOut');
-    buttonLogOut.addEventListener("click", logOut);
+  const buttonLogOut = wallHome.querySelector("#logOut");
+  buttonLogOut.addEventListener("click", logOut);
 
-    return wallHome;
-    }
-    
+  return wallHome;
+};
