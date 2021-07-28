ed14bc62efca7fc06a24af9febc2ab92a81102b3
Mayrenejose <maye199226@gmail.com>
2021-07-28 09:08:09 -0400

    avance en post

--- src/lib/view/templateWall.js
+++ src/lib/view/templateWall.js
@@ -1,28 +1,45 @@
-import {logOut} from './../logOut.js';
+import { logOut } from './../logOut.js';
+import { publishGet } from '../post.js'; // const que se trae los comentarios de la coleccion
 
 export const wall = e => {
 
-    const wallHome = document.createElement('div');
-    const viewWall = `
+  const wallHome = document.createElement('div');
+  const viewWall = `
     <div class="header">
         <h1>Barra superior</h1>
         <button id="logOut">Cerrar Sesión </button>
         </div>
     
     <div class="wallContainer">
-     <div class="postOne">¿Qué Regalas?</div>
-     <div class="postTwo">¿Qué Regalas?</div>
+     <div class="postFull" id='postFull'></div>     
     </div>
     <a href="#/post"> Postear mensaje     
     </a>
-    <div id='postFull'></div>
+    
     `;
 
-    wallHome.innerHTML = viewWall;
+  wallHome.innerHTML = viewWall;
 
-    const buttonLogOut = wallHome.querySelector('#logOut');
-    buttonLogOut.addEventListener("click", logOut);
+  // cierre de sesion
+  const buttonLogOut = wallHome.querySelector('#logOut');
+  buttonLogOut.addEventListener("click", logOut);
 
-    return wallHome;
-    }
-    
+  //llama a coleccion y devuelve promesa
+  publishGet()
+    .then(querySnapshot => {
+      querySnapshot.forEach((doc) => {
+        const postDiv = wallHome.querySelector('#postFull');
+        postDiv.innerHTML = '';
+        postDiv.innerHTML +=
+
+        // aqui crear elementos 
+        console.log('hola may');
+        console.log(doc.id, " => ", doc.data());
+      });
+
+    })
+
+  return wallHome;
+
+}
+
