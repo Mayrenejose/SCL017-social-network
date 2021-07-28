15896801464fffd8638656cfe0fb323bfd496e42
MartaSVinet <isabelle.vinet@gmail.com>
2021-07-28 09:02:26 -0400

    agrega Barra de Navegación por corregir

---
+++ src/lib/templateNavigation.js
@@ -0,0 +1,8 @@
+export const navBar = () => 
+const Navigation = document.createElement('div')
+<div class='header'><img class="iconGoogle" src="Assets/google-mas.png"></img>Eluney</div>
+<div class="functions">
+    <a>¿Quieres postear?</a>
+    <a>Mi Perfil</a>
+    <a>Logout</a>
+</div>
